93131677cb57def49e05eeebffaade53ef10edab
rafael <rafaelhss@gmail.com>
2017-06-13 08:14:00 -0300

    auto descobre schedule

--- src/main/java/com/foodlog/web/rest/BotResource.java
+++ src/main/java/com/foodlog/web/rest/BotResource.java
@@ -23,7 +23,7 @@ public class BotResource {
     @Autowired
     private MealLogService mealLogService;
 
-    private static final String BOT_ID = "374481790:AAHgscpBDG2zs4VsDbeg140VmSVZZeItPEw";
+    private static final String BOT_ID = "380968235:AAGqnrSERR8ABcw-_avcPN2ES3KH5SeZtNM";
 
     @RequestMapping(method= RequestMethod.POST, value="/update")
     public void ReceberUpdate(@RequestBody Update update){
@@ -31,22 +31,26 @@ public class BotResource {
         String mensagem = update.getMessage().getText();
         int user_id = update.getMessage().getFrom().getId();
 
+        String message = "Algum erro aconteceu...";
+
         //testa se recebeu foto
         if(update.getMessage().getPhoto() != null && update.getMessage().getPhoto().size() > 0){
-            System.out.println("fileid:" + update.getMessage().getPhoto().get(0).getFile_id());
-            System.out.println("text:" + update.getMessage().getCaption());
+
             MealLog mealLog = new MealLogFactory(update).create();
-
-            mealLogService.save(mealLog);
-
+            MealLog mealLog1 = mealLogService.save(mealLog);
+            message = "Foto salva com sucesso. Meallog (" + mealLog1.getId() + ") registrado ";
+            if(mealLog1.getScheduledMeal() == null) {
+                message += "sem classificação";
+            } else {
+                message += "como " + mealLog1.getScheduledMeal().getName();
+            }
         } else {
             System.out.println("nao veio foto");
+            message = "Nenhuma foto encontrada na mensagem. Nada fiz...";
         }
 
-
-
         try {
-            new Sender(BOT_ID).sendResponse(user_id, "response");
+            new Sender(BOT_ID).sendResponse(user_id, message);
         } catch (IOException ex) {
             Logger.getLogger(BotResource.class.getName()).log(Level.SEVERE, null, ex);
         }
--- src/main/java/com/foodlog/web/rest/bot/MealLogFactory.java
+++ src/main/java/com/foodlog/web/rest/bot/MealLogFactory.java
@@ -2,8 +2,11 @@ package com.foodlog.web.rest.bot;
 
 
 import com.foodlog.domain.MealLog;
+import com.foodlog.domain.ScheduledMeal;
+import com.foodlog.repository.ScheduledMealRepository;
 import com.foodlog.web.rest.bot.model.GetFile;
 import com.foodlog.web.rest.bot.model.Update;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.client.RestTemplate;
 
 import javax.xml.bind.DatatypeConverter;
@@ -12,11 +15,18 @@ import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.Instant;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 /**
  * Created by rafael on 04/06/17.
  */
 public class MealLogFactory {
+
+    @Autowired
+    ScheduledMealRepository scheduledMealRepository;
+
     private Update update;
 
     public MealLogFactory(Update update) {
@@ -25,11 +35,59 @@ public class MealLogFactory {
 
     public MealLog create() {
         MealLog mealLog = new MealLog();
-        mealLog.setPhotoContentType("image/jpg");
+
+        //Date = now
         mealLog.setMealDateTime(Instant.now());
+
+        //Comment = caption + text
         mealLog.setComment((
                 update.getMessage().getCaption() + " | " +
                         update.getMessage().getText()));
+
+        //Photo
+        byte[] imageBytes = getPicture();
+        mealLog.setPhoto(DatatypeConverter.parseBase64Binary(DatatypeConverter.printBase64Binary(imageBytes)));
+        mealLog.setPhotoContentType("image/jpg");
+
+
+        //ScheduledMeal
+        ScheduledMeal scheduledMeal = defineScheduledMeal(mealLog);
+        mealLog.setScheduledMeal(scheduledMeal);
+
+        return mealLog;
+    }
+
+    private ScheduledMeal defineScheduledMeal(MealLog mealLog) {
+        try {
+            for (ScheduledMeal scheduledMeal : scheduledMealRepository.findAll()) {
+                if(checkTime(scheduledMeal)) {
+                    return scheduledMeal;
+                }
+            }
+        } catch (Exception ex){
+            System.out.println("errrxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    private boolean checkTime(ScheduledMeal scheduledMeal)  {
+        String time[] = scheduledMeal.getTargetTime().split(":");
+
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.of("America/Sao_Paulo"));
+
+        int hour = Integer.parseInt(time[0]);
+        int minute = Integer.parseInt(time[1]);
+        ZonedDateTime target = now.with(LocalTime.of(hour, minute));
+
+        ZonedDateTime after = target.plusMinutes(20);
+        ZonedDateTime before = target.minusMinutes(20);
+
+        return (now.isBefore(after) && now.isAfter(before));
+    }
+
+
+    private byte[] getPicture() {
         int id = update.getMessage().getPhoto().size() -1 ;
         String file_id = update.getMessage().getPhoto().get(id).getFile_id();
 
@@ -45,21 +103,6 @@ public class MealLogFactory {
 
         String file_path = getFile.getResult().getFile_path();
         URI getBytesurl = ApiUrlBuilder.getBytesUrl(file_path);
-        byte[] imageBytes = restTemplate.getForObject(getBytesurl, byte[].class);
-
-
-        mealLog.setPhoto(DatatypeConverter.parseBase64Binary(DatatypeConverter.printBase64Binary(imageBytes)));
-
-
-
-            System.out.println("byte:" + imageBytes);
-            try {
-                Files.write(Paths.get("/home/rafael/Documents/Projects/foodlogbot/target/image.jpg"), imageBytes);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-
-        return mealLog;
+        return restTemplate.getForObject(getBytesurl, byte[].class);
     }
 }
