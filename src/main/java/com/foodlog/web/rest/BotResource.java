c3375d6489ef28cba05234f910bddfb2bfa0a66b
rafael <rafaelhss@gmail.com>
2017-06-16 14:16:36 -0300

    media do tempo

--- src/main/java/com/foodlog/web/rest/BotResource.java
+++ src/main/java/com/foodlog/web/rest/BotResource.java
@@ -1,19 +1,29 @@
 package com.foodlog.web.rest;
 
 import com.foodlog.domain.MealLog;
+import com.foodlog.repository.MealLogRepository;
 import com.foodlog.service.MealLogService;
 import com.foodlog.web.rest.bot.MealLogFactory;
 import com.foodlog.web.rest.bot.model.Update;
 import com.foodlog.web.rest.bot.sender.Sender;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,13 +34,13 @@ import java.util.logging.Logger;
 public class BotResource {
 
     @Autowired
-    private HttpRequest httpRequest;
+    private HttpServletRequest request;
 
-    private List<Long> receivedMessages = new ArrayList<Long>();
+    private List<Long> receivedMessages;// = new ArrayList<Long>();
 
 
     @Autowired
-    private MealLogService mealLogService;
+    private MealLogRepository mealLogRepository;
 
     @Autowired
     private MealLogFactory mealLogFactory;
@@ -39,6 +49,11 @@ public class BotResource {
 
     @RequestMapping(method= RequestMethod.POST, value="/update")
     public void ReceberUpdate(@RequestBody Update update){
+
+        receivedMessages = (List<Long>) request.getSession().getAttribute("receivedMessages");
+        if(receivedMessages == null){
+            receivedMessages = new ArrayList<>();
+        }
 
         String mensagem = update.getMessage().getText();
         int user_id = update.getMessage().getFrom().getId();
@@ -52,13 +67,16 @@ public class BotResource {
                 if (update.getMessage().getPhoto() != null && update.getMessage().getPhoto().size() > 0) {
 
                     MealLog mealLog = mealLogFactory.create(update);
-                    MealLog mealLog1 = mealLogService.save(mealLog);
-                    message = "Foto salva com sucesso. Meallog (" + mealLog1.getId() + ") registrado ";
+                    MealLog mealLog1 = mealLogRepository.save(mealLog);
+                    message = "Foto salva com sucesso, ";
                     if (mealLog1.getScheduledMeal() == null) {
                         message += "sem classificação";
                     } else {
                         message += "como " + mealLog1.getScheduledMeal().getName();
                     }
+
+                    message += calculateMealIntervals();
+
                 } else {
                     System.out.println("nao veio foto");
                     message = "Nenhuma foto encontrada na mensagem. Nada fiz...";
@@ -67,13 +85,44 @@ public class BotResource {
                 new Sender(BOT_ID).sendResponse(user_id, message);
 
                 receivedMessages.add(update.getUpdate_id());
+                request.getSession().setAttribute("receivedMessages", receivedMessages);
 
             } catch (IOException ex) {
                 Logger.getLogger(BotResource.class.getName()).log(Level.SEVERE, null, ex);
             }
-
-
         }
+    }
+
+    private String calculateMealIntervals() {
+
+        try {
+            DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+            Date today = new Date();
+            Date todayWithZeroTime = formatter.parse(formatter.format(today));
+
+            List<MealLog>mealLogs = mealLogRepository.findByMealDateTimeAfterOrderByMealDateTimeDesc(todayWithZeroTime);
+
+            long minutesSum = 0;
+
+            Instant lastMealTime = null;
+            for (MealLog mealLog: mealLogs){
+                if(lastMealTime != null) {
+                    minutesSum += Duration.between(mealLog.getMealDateTime(), lastMealTime).getSeconds() / (60); //minutos
+                }
+                lastMealTime = mealLog.getMealDateTime();
+            }
+
+            Long avg = minutesSum/mealLogs.size();
+
+            System.out.println("meals:"  + mealLogs.size() + " sum:" + minutesSum);
+            
+            return " media de intervalo entre refeicoes: " + avg/60 + " horas";
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return "";
     }
 
     private boolean checkDuplicatedMessage(Long update_id) {
