af1a8e9dfc389bc7591f1ccf092281d7618facc4
rafael <rafaelhss@gmail.com>
2017-07-07 16:19:41 -0300

    timeline funcionando suave localmente

--- src/main/webapp/app/entities/meal-log-day/meal-log-day.controller.js
+++ src/main/webapp/app/entities/meal-log-day/meal-log-day.controller.js
@@ -5,11 +5,14 @@
         .module('foodlogbotadmApp')
         .controller('MealLogDayController', MealLogDayController);
 
-    MealLogDayController.$inject = ['MealLogDay', 'ParseLinks', 'AlertService', 'paginationConstants'];
+    MealLogDayController.$inject = ['DataUtils', 'MealLogDay', 'ParseLinks', 'AlertService', 'paginationConstants', 'VisDataSet', '$scope'];
 
-    function MealLogDayController(MealLogDay, ParseLinks, AlertService, paginationConstants) {
+    function MealLogDayController(DataUtils, MealLogDay, ParseLinks, AlertService, paginationConstants, VisDataSet, $scope) {
 
         var vm = this;
+
+        vm.openFile = DataUtils.openFile;
+
 
         vm.mealLogDays = [];
         vm.loadPage = loadPage;
@@ -38,12 +41,43 @@
                 return result;
             }
 
+            //tudo de custom esta aqui, exceto injecao do $scope e open file
+            function buildTimelineInfo(data) {
+                $scope.timelineDatas = [];
+                $scope.timelineOptions = {
+                  //  height:"100%"
+                };
+                for (var i = 0; i < data.length; i++) {
+                    vm.mealLogDays.push(data[i]);
+                    var timelineData = {};
+                    timelineData.items = extractItens(data[i]);
+                    $scope.timelineDatas.push(timelineData);
+                }
+                function extractItens(data) {
+                    var items = new VisDataSet();
+                    data.mealLogList.forEach(function(item, i){
+                        var start = new Date(item.mealDateTime);
+                        items.add({
+                            id: i,
+                            content:
+                            ' <span style="color:#97B0F8;">' +
+                            '<a  onClick="window.open(\'data:'+ item.photoContentType + ';base64,' + item.photo + '\')\">' +
+                                "<img src=\"data:" + item.photoContentType + ";base64," + item.photo + "\" style=\"max-height: 30px;\" alt=\"mealLog image\"/>" +
+                            '</a>' +
+                             '</span>',
+                            start: start,
+                            type: 'box'
+                            });
+                    })
+                    return items;
+
+                }
+            }
+
             function onSuccess(data, headers) {
                 vm.links = ParseLinks.parse(headers('link'));
                 vm.totalItems = headers('X-Total-Count');
-                for (var i = 0; i < data.length; i++) {
-                    vm.mealLogDays.push(data[i]);
-                }
+                buildTimelineInfo(data);
             }
 
             function onError(error) {
