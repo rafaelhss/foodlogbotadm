f481600fdda33fabb511f4f0ed0e503422e745a0
rafael <rafaelhss@gmail.com>
2017-07-10 20:19:00 -0300

    visualizacao mobile

--- src/main/webapp/app/entities/meal-log/meal-log.controller.js
+++ src/main/webapp/app/entities/meal-log/meal-log.controller.js
@@ -11,6 +11,28 @@
 
         var vm = this;
 
+        String.prototype.toHHMMSS = function () {
+            var sec_num = parseInt(this, 10); // don't forget the second param
+            var hours   = Math.floor(sec_num / 3600);
+            var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+            var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+            if (hours   < 10) {hours   = "0"+hours;}
+            if (minutes < 10) {minutes = "0"+minutes;}
+            if (seconds < 10) {seconds = "0"+seconds;}
+            return hours+':'+minutes+':'+seconds;
+        }
+
+        vm.getTimeBarHeight = function(date1, date2){
+            return '{"height":"'+(new Date(date1) - new Date(date2))/ (1000 * 60)+'px"}';
+        }
+
+        vm.getTimeInMinutes = function (date1, date2){
+          var offset = (new Date(date1) - new Date(date2))/ (1000);
+          offset = Math.floor(offset).toString();
+          return offset.toHHMMSS();
+        }
+
         vm.mealLogs = [];
         vm.loadPage = loadPage;
         vm.itemsPerPage = paginationConstants.itemsPerPage;
